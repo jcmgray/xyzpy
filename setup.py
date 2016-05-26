5d333f7e53922bd3b27ec4d325bb85bbefad27e6
Johnnie Gray <john.gray.14@ucl.ac.uk>
2016-05-26 14:10:45 +0100

    add scipy to requirements

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ setup(name='xyzpy',
       packages=find_packages(exclude=['contrib', 'docs', 'test*']),
       install_requires=[
           'numpy>=1.10.0',
+          'scipy>=0.16.0'
           'xarray>=0.7.0',
           'plotly>=1.9.0',
-          'matplotlib>=1.5.0'],
+          'matplotlib>=1.5.0',
+          ],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5'])
+          'Programming Language :: Python :: 3.5',
+          ])
