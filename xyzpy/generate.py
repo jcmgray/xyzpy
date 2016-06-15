1ae37b5a3ceb316d1bebdeb3b7c81327e5c813ad
jcmgray <john.gray.14@ucl.ac.uk>
2016-06-15 11:41:52 +0100

    NEW: combo_runner and case_runner

--- xyzpy/generate.py
+++ xyzpy/generate.py
@@ -1,7 +1,9 @@
 """
 Generate datasets from function and parameter lists
 """
+
 # TODO: find NaNs in xarray and perform cases
+# TODO: rename --> combo_runner, case_runner
 
 import functools
 import itertools
@@ -23,19 +25,19 @@ def progbar(it, nb=False, **kwargs):
         return tqdm.tqdm(it, **settings)
 
 
-def parse_cases(cases):
-    """ Turn dicts and single tuples into proper form for case runners. """
-    if isinstance(cases, dict):
-        cases = tuple(cases.items())
-    elif isinstance(cases[0], str):
-        cases = (cases,)
+def parse_combos(combos):
+    """ Turn dicts and single tuples into proper form for combo runners. """
+    if isinstance(combos, dict):
+        combos = tuple(combos.items())
+    elif isinstance(combos[0], str):
+        combos = (combos,)
     else:
-        cases = tuple(cases)
-    return cases
+        combos = tuple(combos)
+    return combos
 
 
-def case_runner(fn, cases, constants=None, split=False, progbars=0,
-                parallel=False, processes=None, progbar_opts={}):
+def combo_runner(fn, combos, constants=None, split=False, progbars=0,
+                 parallel=False, processes=None, progbar_opts={}):
     """
     Take a function fn and analyse it over all combinations of named
     variables' values, optionally showing progress and in parallel.
@@ -43,13 +45,13 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
     Parameters
     ----------
         fn: function to analyse
-        cases: list of tuples/dict of form ((variable_name, [values]), ...),
+        combos: list of tuples/dict of form ((variable_name, [values]), ...),
             all combinations of each argument will be calculated. Each
             argument range thus gets a dimension in the output array(s).
         constants: list of tuples/dict of *constant* fn argument mappings.
         split: whether to split into multiple output arrays or not.
         progbars: how many levels of nested progress bars to show.
-        parallel: process cases in parallel.
+        parallel: process combos in parallel.
         processes: how many processes to use, use None for automatic.
         progbar_opts: dict of options for the progress bar.
 
@@ -69,9 +71,9 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
        ...:
        ...:
 
-    In [4]: cases = (('a', [100, 400]), ('b', [20, 50]), ('c', [3, 6]))
+    In [4]: combos = (('a', [100, 400]), ('b', [20, 50]), ('c', [3, 6]))
 
-    In [5]: case_runner(foo, cases, progbars=3)
+    In [5]: combo_runner(foo, combos, progbars=3)
     a: 100%|##########| 2/2 [00:08<00:00,  4.02s/it]
     b: 100%|##########| 2/2 [00:04<00:00,  2.01s/it]
     c: 100%|##########| 2/2 [00:02<00:00,  1.00s/it]
@@ -80,7 +82,7 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
     ((((123, False), (126, True)), ((153, False), (156, True))),
      (((423, False), (426, True)), ((453, False), (456, True))))
 
-    In [6]: case_runner(foo, cases, progbars=3, parallel=True)  # Faster!
+    In [6]: combo_runner(foo, combos, progbars=3, parallel=True)  # Faster!
     a: 100%|##########| 2/2 [00:01<00:00,  1.96it/s]
     b: 100%|##########| 2/2 [00:00<00:00, 327.58it/s]
     c: 100%|##########| 2/2 [00:00<00:00, 17512.75it/s]
@@ -90,7 +92,7 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
      (((423, False), (426, True)), ((453, False), (456, True))))
 
 
-    In [7]: x, y = case_runner(foo, cases, split=True)
+    In [7]: x, y = combo_runner(foo, combos, split=True)
 
     In [8]: x
     Out[8]: (((123, 126), (153, 156)), ((423, 426), (453, 456)))
@@ -100,32 +102,32 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
 
     Notes
     -----
-        1. The parallel evaluation of cases relies on the `multiprocessing`
+        1. The parallel evaluation of combos relies on the `multiprocessing`
             module, this places some restrictions of what functions can be
             used (needs to be picklable/importable).
     """
 
-    # Prepare cases
-    cases = parse_cases(cases)
-    fn_args, _ = zip(*cases)
+    # Prepare combos
+    combos = parse_combos(combos)
+    fn_args, _ = zip(*combos)
 
     # Prepare Function
     if constants is not None:
         fn = functools.partial(fn, **dict(constants))
 
-    # Evaluate cases in parallel
+    # Evaluate combos in parallel
     if parallel or (processes is not None and processes > 1):
         p = multiprocessing.Pool(processes=processes)
 
         # Submit jobs in parallel and in nested structure
-        def submit_jobs(fn, cases, _l=0):
-            arg, inputs = cases[0]
+        def submit_jobs(fn, combos, _l=0):
+            arg, inputs = combos[0]
             for x in inputs:
-                if len(cases) == 1:
+                if len(combos) == 1:
                     yield p.apply_async(fn, kwds={arg: x})
                 else:
                     sub_fn = functools.partial(fn, **{arg: x})
-                    yield tuple(submit_jobs(sub_fn, cases[1:], _l+1))
+                    yield tuple(submit_jobs(sub_fn, combos[1:], _l+1))
 
         # Run through nested structure retrieving results
         def get_outputs(futs, _l=0):
@@ -137,22 +139,22 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
                 else:
                     yield tuple(zip(*get_outputs(fut, _l+1)))
 
-        futures = tuple(submit_jobs(fn, cases))
+        futures = tuple(submit_jobs(fn, combos))
         outputs = tuple(zip(*get_outputs(futures)))
 
-    # Evaluate cases sequentially
+    # Evaluate combos sequentially
     else:
-        def sub_case_runner(fn, cases, _l=0):
-            arg, inputs = cases[0]
+        def get_ouputs_seq(fn, combos, _l=0):
+            arg, inputs = combos[0]
             for x in progbar(inputs, disable=_l >= progbars,
                              desc=arg, **progbar_opts):
-                if len(cases) == 1:
+                if len(combos) == 1:
                     yield fn(**{arg: x}) if split else [fn(**{arg: x})]
                 else:
                     sub_fn = functools.partial(fn, **{arg: x})
-                    yield tuple(zip(*sub_case_runner(sub_fn, cases[1:], _l+1)))
+                    yield tuple(zip(*get_ouputs_seq(sub_fn, combos[1:], _l+1)))
 
-        outputs = tuple(zip(*sub_case_runner(fn, cases)))
+        outputs = tuple(zip(*get_ouputs_seq(fn, combos)))
 
     # Make sure left progress bars are not writter over
     if progbars > 0:
@@ -162,60 +164,17 @@ def case_runner(fn, cases, constants=None, split=False, progbars=0,
     return outputs if split else outputs[0]
 
 
-def xr_case_runner(fn, cases, var_names, var_dims=([],),
-                   var_coords={},**kwargs):
-    """
-    Take a function fn and analyse it over all combinations of named
-    variables values, optionally showing progress and outputing to xarray.
-
-    Parameters
-    ----------
-        fn: function to analyse
-        cases: list of tuples of form ((variable_name, [values]), ...)
-        var_dims, name of dataset's main variable, i.e. the results of fn
-        **kwargs: are passed to `case_runner`
-
-    Returns
-    -------
-        ds: xarray Dataset with appropirate coordinates.
-    """
-
-    # Prepare cases
-    cases = tuple(cases.items() if isinstance(cases, dict) else cases)
-    case_names, _ = zip(*cases)
-
-    # Work out if multiple variables are expected as output
-    if isinstance(var_names, str):
-        split = False
-        var_names = (var_names,)
-        var_dims = (var_dims,)
-    elif len(var_names) == 1:
-        split = False
-    else:
-        split = True
-        var_dims = itertools.cycle(var_dims)
-
-    # Generate the data
-    vdatas = case_runner(fn, cases, split=split, **kwargs)
-    if not split:
-        vdatas = (vdatas,)
-
-    # Set dataset coordinates
-    ds = xr.Dataset(coords={**dict(cases), **dict(var_coords)})
-
-    # Set Dataset dataarrays
-    for vdata, vname, vdims in zip(vdatas, var_names, var_dims):
-        ds[vname] = (tuple(case_names) + tuple(vdims), np.asarray(vdata))
-
-    return ds
+def combos_to_ds():
+    pass
 
 
-def config_runner(fn, fn_args, configs, constants=None, split=False,
-                  progbars=0, parallel=False, processes=None, progbar_opts={}):
+def case_runner(fn, fn_args, cases, constants=None, split=False,
+                progbars=0, parallel=False, processes=None, progbar_opts={}):
     """
     Evaluate a function in many different configurations, optionally in
     parallel and or with live progress.
     """
+
     # Prepare Function
     if constants is not None:
         fn = functools.partial(fn, **dict(constants))
@@ -223,20 +182,143 @@ def config_runner(fn, fn_args, configs, constants=None, split=False,
     # Prepate fn_args and values
     if isinstance(fn_args, str):
         fn_args = (fn_args,)
-        configs = tuple((c,) for c in configs)
+        cases = tuple((c,) for c in cases)
 
     # Evaluate configurations in parallel
     if parallel or (processes is not None and processes > 1):
         p = multiprocessing.Pool(processes=processes)
         fut = tuple(p.apply_async(fn, kwds=dict(zip(fn_args, c)))
-                    for c in configs)
-        xs = tuple(x.get() for x in progbar(fut, total=len(configs),
-                                            disable=progbars<1))
+                    for c in cases)
+        xs = tuple(x.get() for x in progbar(fut, total=len(cases),
+                                            disable=progbars < 1))
 
     # Evaluate configutation sequentially
     else:
         xs = tuple(fn(**{arg: y for arg, y in zip(fn_args, cnfg)})
-                   for cnfg in progbar(configs, total=len(configs),
-                                       disable=progbars<1))
+                   for cnfg in progbar(cases, total=len(cases),
+                                       disable=progbars < 1))
 
     return xs if not split else tuple(zip(*xs))
+
+
+def combo_runner_to_ds(fn, combos, var_names, var_dims=([],), var_coords={},
+                       **kwargs):
+    """
+    Take a function fn and analyse it over all combinations of named
+    variables values, optionally showing progress and outputing to xarray.
+
+    Parameters
+    ----------
+        fn: function to analyse
+        combos: list of tuples of form ((variable_name, [values]), ...) with
+            which to generate all function evaluation configurations.
+        var_names: name(s) of output variables of `fn`
+        var_dims: the list of named output coordinates for each output
+            variable, i.e. coordinates not generated by the combo_runner
+        var_coords: dict of values for those coordinates if custom ones are
+            desired.
+        **kwargs: are passed to `combo_runner`
+
+    Returns
+    -------
+        ds: xarray Dataset with appropirate coordinates.
+
+    Examples
+    --------
+    In [1]: from xyzpy import *
+
+    In [2]: def foo(a, b, c):
+       ...:     return a + b + c, (a + b + c) % 2 == 0
+       ...:
+       ...:
+
+    In [3]: var_names = ['total', 'is_even']
+
+    In [4]: combos = [('a', [1, 2, 3]),
+                     ('b', [10, 20, 30]),
+                     ('c', [100, 200, 300])]
+
+    In [5]: combo_runner_to_ds(foo, combos, var_names)
+    Out[5]:
+    <xarray.Dataset>
+    Dimensions:  (a: 3, b: 3, c: 3)
+    Coordinates:
+      * b        (b) int64 10 20 30
+      * a        (a) int64 1 2 3
+      * c        (c) int64 100 200 300
+    Data variables:
+        total    (a, b, c) int64 111 211 311 121 221 321 131 231 331 112 ...
+        is_even  (a, b, c) bool False False False False False False False ...
+    """
+
+    # TODO: split out data preperation
+
+    # Prepare combos
+    combos = tuple(combos.items() if isinstance(combos, dict) else combos)
+    fn_args, _ = zip(*combos)
+
+    # Work out if multiple variables are expected as output
+    if isinstance(var_names, str):
+        split = False
+        var_names = (var_names,)
+        if var_dims != ([],):
+            var_dims = (var_dims,)
+    elif len(var_names) == 1:
+        split = False
+    else:
+        split = True
+    var_dims = itertools.cycle(var_dims)
+
+    # Generate the data
+    vdatas = combo_runner(fn, combos, split=split, **kwargs)
+    if not split:
+        vdatas = (vdatas,)
+
+    # Set dataset coordinates
+    ds = xr.Dataset(coords={**dict(combos), **dict(var_coords)})
+
+    # Set Dataset dataarrays
+    for vdata, vname, vdims in zip(vdatas, var_names, var_dims):
+        ds[vname] = (tuple(fn_args) + tuple(vdims), np.asarray(vdata))
+
+    return ds
+
+
+def cases_to_ds(results, configs, case_names, var_names,
+                var_dims=([],), var_coords={}):
+
+    # Prepare case_names/configs var_names/results
+    if isinstance(case_names, str):
+        case_names = (case_names,)
+        configs = tuple((c,) for c in configs)
+    if isinstance(var_names, str):
+        var_names = (var_names,)
+        results = tuple((r,) for r in results)
+        if var_dims != ([],):
+            var_dims = (var_dims,)
+    var_dims = itertools.cycle(var_dims)
+
+    # Find required coordinates from configs
+    c_coords = {arg: sorted([*{*arg_vals}])
+                for arg, arg_vals in zip(case_names, zip(*configs))}
+
+    # Add dimensions from cases and var_coords
+    ds = xr.Dataset(coords={**c_coords, **dict(var_coords)})
+    shape_c_coords = [ds[dim].size for dim in case_names]
+
+    # go through var_names, adding np.nan in correct shape
+    for vname, dims in zip(var_names, var_dims):
+        vdims = tuple(case_names) + tuple(dims)
+        shape = shape_c_coords + [ds[d].size for d in dims]
+        ds[vname] = (vdims, np.tile(np.nan, shape))
+
+    #  go through configs, overwriting nan with results
+    for res, cfg in zip(results, configs):
+        for vname, x in zip(var_names, res):
+            ds[vname].loc[dict(zip(case_names, cfg))] = x
+
+    return ds
+
+
+def case_runner_to_ds():
+    pass
--- xyzpy/tests/test_generate.py
+++ xyzpy/tests/test_generate.py
@@ -7,9 +7,10 @@ from numpy.testing import assert_allclose
 
 from ..generate import (
     progbar,
+    combo_runner,
+    combo_runner_to_ds,
     case_runner,
-    config_runner,
-    xr_case_runner,
+    cases_to_ds
 )
 
 
@@ -59,49 +60,49 @@ class TestProgbar:
             pass
 
 
-class TestCaseRunner:
+class TestcomboRunner:
     def test_simple(self):
-        cases = [('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400])]
-        x = case_runner(foo1, cases)
+        combos = [('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400])]
+        x = combo_runner(foo1, combos)
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
         assert_allclose(x, xn)
 
     def test_progbars(self):
-        cases = [('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400])]
-        case_runner(foo1, cases, progbars=3)
+        combos = [('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400])]
+        combo_runner(foo1, combos, progbars=3)
 
     def test_dict(self):
-        cases = OrderedDict((('a', [1, 2]),
+        combos = OrderedDict((('a', [1, 2]),
                              ('b', [10, 20, 30]),
                              ('c', [100, 200, 300, 400])))
-        x = case_runner(foo1, cases)
+        x = combo_runner(foo1, combos)
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
         assert_allclose(x, xn)
 
-    def test_single_case(self):
-        cases = [('a', [1, 2])]
-        x = case_runner(partial(foo1, b=20, c=300), cases)
+    def test_single_combo(self):
+        combos = [('a', [1, 2])]
+        x = combo_runner(partial(foo1, b=20, c=300), combos)
         assert_allclose(x, [321, 322])
 
-    def test_single_case_single_tuple(self):
-        cases = ('a', [1, 2])
+    def test_single_combo_single_tuple(self):
+        combos = ('a', [1, 2])
         constants = {'b': 20, 'c': 300}
-        x = case_runner(foo1, cases, constants=constants)
+        x = combo_runner(foo1, combos, constants=constants)
         assert_allclose(x, [321, 322])
 
     def test_multires(self):
-        cases = [('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400])]
-        x, y = case_runner(foo2, cases, split=True)
+        combos = [('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400])]
+        x, y = combo_runner(foo2, combos, split=True)
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
@@ -111,22 +112,22 @@ class TestCaseRunner:
         assert_allclose(y, yn)
 
 
-class TestParallelCaseRunner:
+class TestParallelcomboRunner:
     def test_basic(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400]))
-        x = case_runner(foo1, cases, processes=2)
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400]))
+        x = combo_runner(foo1, combos, processes=2)
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
         assert_allclose(x, xn)
 
     def test_multires(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400]))
-        x = case_runner(foo2, cases, processes=2, split=True)
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400]))
+        x = combo_runner(foo2, combos, processes=2, split=True)
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
@@ -134,29 +135,29 @@ class TestParallelCaseRunner:
         assert np.all(np.asarray(x[1])[1, ...])
 
     def test_dict(self):
-        cases = OrderedDict((('a', [1, 2]),
+        combos = OrderedDict((('a', [1, 2]),
                              ('b', [10, 20, 30]),
                              ('c', [100, 200, 300, 400])))
-        x = [*case_runner(foo1, cases, processes=2)]
+        x = [*combo_runner(foo1, combos, processes=2)]
         xn = (np.array([1, 2]).reshape((2, 1, 1)) +
               np.array([10, 20, 30]).reshape((1, 3, 1)) +
               np.array([100, 200, 300, 400]).reshape((1, 1, 4)))
         assert_allclose(x, xn)
 
 
-class TestXRCaseRunner:
+class TestXRcomboRunner:
     def test_basic(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400]))
-        ds = xr_case_runner(foo1, cases, var_names=['bananas'])
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400]))
+        ds = combo_runner_to_ds(foo1, combos, var_names=['bananas'])
         assert ds.sel(a=2, b=30, c=400)['bananas'].data == 432
 
     def test_multiresult(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]),
-                 ('c', [100, 200, 300, 400]))
-        ds = xr_case_runner(foo2, cases, var_names=['bananas', 'cakes'])
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]),
+                  ('c', [100, 200, 300, 400]))
+        ds = combo_runner_to_ds(foo2, combos, var_names=['bananas', 'cakes'])
         assert ds.bananas.data.dtype == int
         assert ds.cakes.data.dtype == bool
         assert ds.sel(a=2, b=30, c=400)['bananas'].data == 432
@@ -165,24 +166,24 @@ class TestXRCaseRunner:
         assert not ds.sel(a=1, b=10, c=100)['cakes'].data
 
     def test_arrayresult(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]))
-        ds = xr_case_runner(foo_t, cases,
-                            var_names='bananas',
-                            var_dims=(['sugar']),
-                            var_coords={'sugar': [*range(10)]})
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]))
+        ds = combo_runner_to_ds(foo_t, combos,
+                                var_names='bananas',
+                                var_dims=(['sugar']),
+                                var_coords={'sugar': [*range(10)]})
         assert ds.bananas.data.dtype == float
         assert_allclose(ds.sel(a=2, b=30)['bananas'].data,
                         [32.0, 32.1, 32.2, 32.3, 32.4,
                          32.5, 32.6, 32.7, 32.8, 32.9])
 
     def test_array_and_single_result(self):
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]))
-        ds = xr_case_runner(foo_t2, cases,
-                            var_names=['bananas', 'ripe'],
-                            var_dims=(['sugar'], []),
-                            var_coords={'sugar': [*range(10, 20)]})
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]))
+        ds = combo_runner_to_ds(foo_t2, combos,
+                                var_names=['bananas', 'ripe'],
+                                var_dims=(['sugar'], []),
+                                var_coords={'sugar': [*range(10, 20)]})
         assert ds.ripe.data.dtype == bool
         assert ds.sel(a=2, b=30, sugar=14)['bananas'].data == 32.4
         with raises(KeyError):
@@ -190,16 +191,20 @@ class TestXRCaseRunner:
 
     def test_double_array_return_with_same_dimensions(self):
         def foo(a, b):
-            return [a + i*b for i in range(5)], [a - i*b for i in range(5)]
-        cases = (('a', [1, 2]),
-                 ('b', [10, 20, 30]))
-        ds = xr_case_runner(foo, cases,
-                            var_names=['apples', 'oranges'],
-                            var_dims=(['seeds'],),
-                            var_coords={'seeds': [*range(5)]})
+            return [b + i*a for i in range(5)], [b - i*a for i in range(5)]
+        combos = (('a', [1, 2]),
+                  ('b', [10, 20, 30]))
+        ds = combo_runner_to_ds(foo, combos,
+                                var_names=['apples', 'oranges'],
+                                var_dims=(['seeds'],),
+                                var_coords={'seeds': [*range(5)]})
         assert ds.oranges.data.dtype == int
+        assert_allclose(ds.sel(a=2, b=30).apples.data, [30, 32, 34, 36, 38])
+        assert_allclose(ds.sel(a=2, b=30).oranges.data, [30, 28, 26, 24, 22])
         assert 'seeds' in ds.apples.coords
         assert 'seeds' in ds.oranges.coords
+
+        # TODO: test single string var_names and no given var_dims
 
 
 class TestConfigRunner:
@@ -207,41 +212,94 @@ class TestConfigRunner:
         configs = ((1, 10, 100),
                    (2, 20, 200),
                    (3, 30, 300))
-        xs = config_runner(foo1, ('a', 'b', 'c'), configs)
+        xs = case_runner(foo1, ('a', 'b', 'c'), configs)
         assert xs == (111, 222, 333)
 
     def test_progbar(self):
         configs = ((1, 10, 100),
                    (2, 20, 200),
                    (3, 30, 300))
-        xs = config_runner(foo1, ('a', 'b', 'c'), configs, progbars=1)
+        xs = case_runner(foo1, ('a', 'b', 'c'), configs, progbars=1)
         assert xs == (111, 222, 333)
 
     def test_constants(self):
         configs = ((1,),
                    (2,),
                    (3,))
-        xs = config_runner(foo1, ('a', 'b', 'c'), configs,
-                           constants={'b': 10, 'c': 100})
+        xs = case_runner(foo1, ('a', 'b', 'c'), configs,
+                         constants={'b': 10, 'c': 100})
         assert xs == (111, 112, 113)
 
     def test_parallel(self):
         configs = ((1, 10, 100),
                    (2, 20, 200),
                    (3, 30, 300))
-        xs = config_runner(foo1, ('a', 'b', 'c'), configs, processes=2)
+        xs = case_runner(foo1, ('a', 'b', 'c'), configs, processes=2)
         assert xs == (111, 222, 333)
 
     def test_split(self):
         configs = ((1, 10, 100),
                    (2, 20, 200),
                    (3, 30, 300))
-        a, b = config_runner(foo2, ('a', 'b', 'c'), configs, split=True)
+        a, b = case_runner(foo2, ('a', 'b', 'c'), configs, split=True)
         assert a == (111, 222, 333)
         assert b == (False, True, False)
 
     def test_single_args(self):
         configs = (1, 2, 3)
-        xs = config_runner(foo1, 'a', configs,
-                           constants={'b': 10, 'c': 100})
+        xs = case_runner(foo1, 'a', configs,
+                         constants={'b': 10, 'c': 100})
         assert xs == (111, 112, 113)
+
+
+class TestResConfigsToDS:
+    def test_simple(self):
+        results = ((1,), (2,), (3,), (4,), (5,))
+        configs = (('a', 'a'), ('a', 'b'), ('b', 'c'), ('c', 'c'), ('b', 'a'))
+        var_names = ['bananas']
+        ds = cases_to_ds(results=results,
+                         configs=configs,
+                         case_names=['case1', 'case2'],
+                         var_names=var_names)
+        assert_allclose(ds.bananas.data, [[1, 2, np.nan],
+                                          [5, np.nan, 3],
+                                          [np.nan, np.nan, 4]])
+
+    def test_single_result_format(self):
+        results = [1, 2, 3, 4, 5]
+        configs = [('a', 'a'), ('a', 'b'), ('b', 'c'), ('c', 'c'), ('b', 'a')]
+        var_names = 'bananas'
+        ds = cases_to_ds(results=results,
+                         configs=configs,
+                         case_names=['case1', 'case2'],
+                         var_names=var_names)
+        assert_allclose(ds.bananas.data, [[1, 2, np.nan],
+                                          [5, np.nan, 3],
+                                          [np.nan, np.nan, 4]])
+
+    def test_single_cases_format(self):
+        results = [1, 2, 3, 4, 5]
+        configs = ['a', 'b', 'c', 'd', 'e']
+        var_names = 'bananas'
+        ds = cases_to_ds(results=results,
+                         configs=configs,
+                         case_names='case1',
+                         var_names=var_names)
+        assert_allclose(ds.bananas.data, [1, 2, 3, 4, 5])
+
+    def test_multires(self):
+        var_names = 'lists'
+        var_vals = [np.arange(10) + i for i in range(5)]
+        var_dims = ['time']
+        var_coords = {'time': np.arange(10) / 10}
+        case_names = 'letter'
+        case_cnfgs = ['a', 'b', 'c', 'd', 'e']
+
+        ds = cases_to_ds(results=var_vals,
+                         configs=case_cnfgs,
+                         case_names=case_names,
+                         var_names=var_names,
+                         var_dims=var_dims,
+                         var_coords=var_coords)
+
+        assert ds.time.data.dtype == float
