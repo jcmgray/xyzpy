b17d41d0fdbddfe256ae60c8a061b4ed42a5916e
Johnnie Gray <john.gray.14@ucl.ac.uk>
2018-08-06 15:48:35 +0100

    update travis stuff

--- deps/travis-install-conda.sh
+++ deps/travis-install-conda.sh
@@ -2,10 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# check if conda hasn't been installed (cached) yet
 if [ ! -d "$HOME/conda/bin" ]; then
+
+  # make sure no conda folder just in case (sometimes leftover?)
   if [ -d "$HOME/conda" ]; then
     rm -rf $HOME/conda
   fi
+
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/conda
   export PATH="$HOME/conda/bin:$PATH"
@@ -16,9 +20,6 @@ if [ ! -d "$HOME/conda/bin" ]; then
   conda env create --name test-environment --file $DIR/requirements-py3.yml
   source activate test-environment
 
-  # need up-to-date xarray
-  pip install git+git://github.com/pydata/xarray.git -U --no-deps
-
 else
   export PATH="$HOME/conda/bin:$PATH"
   hash -r
@@ -27,6 +28,6 @@ else
   source activate test-environment
   conda update -q --file $DIR/requirements-py3.yml
 
-  # continous integration, coverage etc.
+  # update non-conda packages
   pip install -U codeclimate-test-reporter
 fi
